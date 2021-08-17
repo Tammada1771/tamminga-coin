71fd117bf72c811465e79de03f79e3affafdbab9
Adam Tamminga <tammingaar28@gmail.com>
2021-08-17 18:48:09 -0500

    Set up the initial mining reward for successfull miners

--- backend/tests/wallet/test_transaction.py
+++ backend/tests/wallet/test_transaction.py
@@ -1,3 +1,4 @@
+from backend.config import MINING_REWARD, MINING_REWARD_INPUT
 import pytest
 
 from backend.wallet.transaction import Transaction
@@ -91,3 +92,11 @@ def test_valid_transaction_with_invalid_signature():
 
     with pytest.raises(Exception, match='Invalid signature'):
         Transaction.is_valid_transaction(transaction)
+
+
+def test_reward_transaction():
+    miner_wallet = Wallet()
+    transaction = Transaction.reward_transaction(miner_wallet)
+
+    assert transaction.input == MINING_REWARD_INPUT
+    assert transaction.output[miner_wallet.address] == MINING_REWARD
